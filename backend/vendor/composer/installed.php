e765dffe5334d2f84b1c879047107e4fa30bd818
Maxime-Francois <maximefrancois92@gmail.com>
2021-01-20 13:48:54 +0100

    frontend effects

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a006661acdee37f91a6584f2386366e825bc2f0a',
+    'name' => 'fabio/pokedex',
+  ),
+  'versions' => 
+  array (
+    'altorouter/altorouter' => 
+    array (
+      'pretty_version' => '2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '127f6e96998708a31ef32252985bea82e3b03888',
+    ),
+    'benoclock/alto-dispatcher' => 
+    array (
+      'pretty_version' => '1.1',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aa05d13d92ae7ac984ec4e63ff439f57fe1efb3d',
+    ),
+    'fabio/pokedex' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a006661acdee37f91a6584f2386366e825bc2f0a',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.18.1',
+      'version' => '1.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a6977d63bf9a0ad4c65cd352709e230876f9904a',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.18.1',
+      'version' => '1.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd87d5766cbf48d72388a9f6b85f280c8ad51f981',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.1.5',
+      'version' => '5.1.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b43a3905262bcf97b2510f0621f859ca4f5287be',
+    ),
+  ),
+);
